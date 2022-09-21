f357f3f6fc521b6fe9ac1f482003622b920d4838
littlepm96 <poscentemarco@hotmail.it>
2022-09-21 17:44:52 +0200

    Initial commit

---
+++ src/main/java/com/example/locoweb/LocoWebApplication.java
@@ -0,0 +1,14 @@
+package com.example.locoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class LocoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/locoweb/LocoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
