0d759fee3a277ea622b1d1ae8d91b6662ccdb3bf
Lindbrum <ultrascati95@gmail.com>
2022-09-23 19:07:51 +0200

    Scomposizione index.html in header.html, home-content.html e footer.html

--- src/main/java/com/example/locoweb/LocoWebApplication.java
+++ src/main/java/com/example/locoweb/LocoWebApplication.java
@@ -1,7 +1,10 @@
 package com.example.locoweb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
@@ -9,28 +12,10 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import javax.servlet.ServletContext;
 
 @SpringBootApplication
+@EnableWebMvc
+@AutoConfiguration
+@ComponentScan(basePackages = {"controller", "model", "service", "microservice"})
 public class LocoWebApplication {
-
-    private final TemplateEngine templateEngine;
-
-    public LocoWebApplication(final ServletContext servletContext){
-        super();
-        //Setup del template engine
-        ServletContextTemplateResolver templateResolver =
-                new ServletContextTemplateResolver(servletContext);
-
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        // Converte "home" a "/WEB-INF/templates/home.html"
-        templateResolver.setPrefix("/WEB-INF/templates/");
-        templateResolver.setSuffix(".html");
-        // Mantieni template in cache per 1 ora.
-        templateResolver.setCacheTTLMs(3600000L);
-
-        templateResolver.setCacheable(true);
-
-        this.templateEngine = new TemplateEngine();
-        this.templateEngine.setTemplateResolver(templateResolver);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(LocoWebApplication.class, args);
